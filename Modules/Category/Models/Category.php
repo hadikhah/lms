d012d643fa729871987efe6e123094e042fe8444
hadikhah <hadikhahe@gmail.com>
2024-02-12 22:06:53 +0100

    refactor Category model
    add docs and fix typos

--- Modules/Category/Models/Category.php
+++ Modules/Category/Models/Category.php
@@ -1,29 +1,55 @@
 <?php
+
 namespace Modules\Category\Models;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Translation\Translator;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Modules\Course\Models\Course;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $guarded= [];
+    protected $guarded = [];
 
-    public function getParentAttribute()
+    /**
+     * returns parent category title
+     *
+     * @return array|Application|Translator|\Illuminate\Foundation\Application|string|null
+     */
+    public function getParentAttribute(): \Illuminate\Foundation\Application|array|string|Translator|Application|null
     {
-        return (is_null($this->parent_id)) ? 'ندارند' : $this->parentCategory->title;
+        return (is_null($this->parent_id)) ? __("none") : $this->parentCategory->title;
     }
 
-    public function parentCategory()
+    /**
+     * parent category relation
+     *
+     * @return BelongsTo
+     */
+    public function parentCategory(): BelongsTo
     {
         return $this->belongsTo(Category::class, 'parent_id');
     }
 
-    public function subCategories()
+    /**
+     * sub category relation
+     *
+     * @return HasMany
+     */
+    public function subCategories(): HasMany
     {
         return $this->hasMany(Category::class, 'parent_id');
     }
 
-    public function courses()
+    /**
+     * courses relation
+     * each category has many courses
+     *
+     * @return HasMany
+     */
+    public function courses(): HasMany
     {
         return $this->hasMany(Course::class);
     }
