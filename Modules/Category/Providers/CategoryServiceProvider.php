13054888e7185ebd03b66de614c0aa68e5d6dca9
hadikhah <hadikhahe@gmail.com>
2024-02-12 22:27:53 +0100

    fix load json translation in category service provider

--- Modules/Category/Providers/CategoryServiceProvider.php
+++ Modules/Category/Providers/CategoryServiceProvider.php
@@ -24,7 +24,7 @@ class CategoryServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/../Routes/categories_routes.php');
         $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'Categories');
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
-        $this->loadJsonTranslationsFrom(__DIR__, "/../lang");
+        $this->loadJsonTranslationsFrom(__DIR__, "/../Resources/Lang");
         DatabaseSeeder::$seeders[] = CategorySeeder::class;
         Gate::policy(Category::class, CategoryPolicy::class);
     }
